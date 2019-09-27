571f81cd318289906fc86aab7dac7ea69238b259
KranK <krankshuffle@gmail.com>
2019-09-28 01:58:42 +0300

    Add Touch Functionality
    Added full touch support on the main Menu
    
    TODO:
    Add Touch to the message boxes :)

--- source/main.c
+++ source/main.c
@@ -64,6 +64,7 @@ int main(int argc, char **argv)
 
     // set the cursor position to 0.
     short cursor = 0;
+    u32 prev_touchcount = 0;
 
     // TODO: touch
     /*u32 tch = 0;
@@ -74,6 +75,7 @@ int main(int argc, char **argv)
     // muh loooooop
     while(appletMainLoop())
     {
+        
         hidScanInput();
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
@@ -121,9 +123,88 @@ int main(int argc, char **argv)
             }
         }
         
-        // exit...
+        // exit
         if (kDown & KEY_PLUS) break;
 
+        // oh boi o love touch so much, KranK touched this. loops are also cool :P
+        // read touch inputs / touch goes before hid inputs ...
+        
+        touchPosition touch;
+        u32 touch_count = hidTouchCount();
+
+        if (touch_count != prev_touchcount)
+        {
+            prev_touchcount = touch_count;
+
+        }
+
+        for(u32 i = 0; i < touch_count; i++)
+        {
+            //Read the touch screen coordinates
+            hidTouchRead(&touch, i);
+            
+        }
+
+        // A key press by touch
+        if (touch.px > 1010 && touch.py > 675  && touch.px < 1100)
+        {
+            switch (cursor)
+            {
+            case UP_AMS:
+                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+                break;
+
+            case UP_AMS_NOINI:
+                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+                break;
+
+            case UP_HEKATE:
+                update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
+                break;
+
+            case UP_APP:
+                update_app();
+                break;
+
+            case REBOOT_PAYLOAD:
+                reboot_payload("/atmosphere/reboot_payload.bin");
+                break;
+            }
+           
+        }
+
+        // actual option press by touch
+        else if (touch.px > 500 && touch.py > 130 && touch.py < 180 && touch.px < 1200)
+        {
+            cursor = UP_AMS;
+            update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+        }
+        else if (touch.px > 500 && touch.py > 230 && touch.py < 280 && touch.px < 1200)
+        {
+            cursor = UP_AMS_NOINI;
+            update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+        }
+        else if (touch.px > 500 && touch.py > 330 && touch.py < 380 && touch.px < 1200)
+        {
+            cursor = UP_HEKATE;
+            update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
+        }
+        else if (touch.px > 500 && touch.py > 430 && touch.py < 480 && touch.px < 1200)
+        {
+            cursor = UP_APP;
+            update_app();
+        }
+        else if (touch.px > 500 && touch.py > 530 && touch.py < 580 && touch.px < 1200)
+        {
+            cursor = REBOOT_PAYLOAD;
+            reboot_payload("/atmosphere/reboot_payload.bin");
+        }
+        
+        // exit by touch 
+        else if (touch.px > 1200 && touch.py > 675  && touch.px < 1240) break;
+
+        // reset last touch
+        touch.px = 0;
         // display render buffer
         updateRenderer();
     }
