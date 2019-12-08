40aa3c74ee70b48003f7b572c6449b4ba9fa6487
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2019-12-08 01:21:26 +0000

    quick fix to allow downloading of pre releases

--- includes/download.h
+++ includes/download.h
@@ -1,7 +1,7 @@
 #ifndef _DOWNLOAD_H_
 #define _DOWNLOAD_H_
 
-#define AMS_URL     "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases/latest"
+#define AMS_URL     "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases"
 #define HEKATE_URL  "https://api.github.com/repos/CTCaer/hekate/releases/latest"
 #define APP_URL     "https://github.com/ITotalJustice/atmosphere-updater/releases/latest/download/atmosphere-updater.nro"
 
